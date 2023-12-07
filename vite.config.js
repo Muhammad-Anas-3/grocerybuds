3023c676343d4bed6463bab0d85d6241707140af
Muhammad-Anas-3 <134198144+Muhammad-Anas-3@users.noreply.github.com>
2023-12-07 09:36:30 +0500

    updated

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'https://github.com/Muhammad-Anas-3/grocerybuds',
+  base: 'https://github.com/Muhammad-Anas-3/grocerybuds',
+  server: {
+    proxy: {
+      '/api': 'https://github.com/Muhammad-Anas-3/grocerybuds'
+    }
+  },
   plugins: [react()],
 })
